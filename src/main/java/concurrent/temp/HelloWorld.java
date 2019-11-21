2c0565fe87bc7638a4f74d5ebe89244422a69452
lihaoyu <825091124@qq.com>
2019-11-21 13:49:25 +0800

    算法第四次课堂练习

--- src/main/java/concurrent/temp/HelloWorld.java
+++ src/main/java/concurrent/temp/HelloWorld.java
@@ -1,14 +1,15 @@
 package concurrent.temp;
 
+import java.util.BitSet;
+
 /**
  * @author lihaoyu
  * @date 2019/11/9 11:34
  */
-public class HelloWorld extends Thread {
+public class HelloWorld{
 
-    private Runnable target;
+public static void main(String[] args) {
+    BitSet bitSet = new BitSet(11);
+}
 
-    public static void main(String[] args) {
-        System.out.println("");
-    }
 }
