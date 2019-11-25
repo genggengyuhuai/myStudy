13ca01aa1450951a71f1db205c41a90a09a364e3
lihaoyu <825091124@qq.com>
2019-11-25 13:54:59 +0800

    算法作业

--- src/main/java/algorithm/temp/Solution.java
+++ src/main/java/algorithm/temp/Solution.java
@@ -1,28 +1,40 @@
 package algorithm.temp;
 
-
-import java.math.BigInteger;
-import java.util.Scanner;
-
-/**
- * @author lihaoyu
- * @date 2019/9/29 11:05
- */
-
-
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Solution {
-
-    public static void main(String[] args) {
-        int N = 100;
-        int[] dp = new int[N+1];
-        int[] weight = new int[]{6,8,10};
-        for(int i = 0; i < weight.length; i++){
-            for(int j = weight[i]; j < dp.length; j++){
-                dp[i] = Math.max(dp[j], dp[j-weight[i]]+weight[i]);
-            }
+    public ArrayList<ArrayList<Integer> > fun(int sum) {
+        int temp = 0, x = 0;
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if(sum <= 0){
+            return res;
         }
+        for(int i = 1;i*(i+1)/2 <= sum;i++){
+            // i 是项数
+            if(2*sum % i != 0){
+                continue;
+            }
+            temp = 2*sum / i;
+            if(((temp - i + 1) & 1) == 0){
+                //  x是序列起始值
+                x = (temp - i + 1) / 2;
+                ArrayList<Integer> tempList = new ArrayList<>();
+                for(int j = x; j < x+i;j++){
+                    tempList.add(j);
+                }
+                if(tempList.size() > 1){
+                    res.add(tempList);
+                }
+            }
 
+        }
+        Collections.reverse(res);
+        return res;
     }
 
+    public static void main(String[] args) {
+Solution solution = new Solution();
+        System.out.println(solution.fun(100));
+    }
 }
