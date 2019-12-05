284e7857bb3d84eb11abc0ed21de35be0e67ec08
lihaoyu <825091124@qq.com>
2019-12-05 21:11:34 +0800

    设计模式复习，明天考试

--- src/main/java/concurrent/temp/HelloWorld.java
+++ src/main/java/concurrent/temp/HelloWorld.java
@@ -1,17 +1,11 @@
 package concurrent.temp;
 
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
 /**
  * @author lihaoyu
  * @date 2019/11/9 11:34
  */
 public class HelloWorld{
-public static void main(String[] args) throws FileNotFoundException {
-    InputStream in = new BufferedInputStream(new FileInputStream(""));
+public static void main(String[] args) {
 }
 }
---
+++ src/main/java/design_pattern/prototype/PrototypeTest.java
@@ -0,0 +1,35 @@
+package design_pattern.prototype;
+
+/**
+ * 原型模式，复制对象，从缓冲池中取个对象然后复制返回
+ *
+ * @author lihaoyu
+ * @date 2019/12/5 19:29
+ */
+
+class Attachment{
+    String url;
+}
+
+class Email implements Cloneable{
+    String title;
+    Attachment attachment;
+
+    @Override
+    protected Object clone() {
+        Email email = new Email();
+        email.title = this.title;
+        return email;
+    }
+}
+
+
+public class PrototypeTest {
+
+    public static void main(String[] args)  {
+
+
+
+    }
+
+}
