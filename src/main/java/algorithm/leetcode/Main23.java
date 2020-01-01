031eae122d116bf70383fb8a8d415c3ad8b88fa6
lihaoyu <825091124@qq.com>
2020-01-01 17:15:33 +0800

    leetcode 动归

---
+++ src/main/java/algorithm/leetcode/Main23.java
@@ -0,0 +1,18 @@
+package algorithm.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author lihaoyu
+ * @date 2020/1/1 13:07
+ */
+public class Main23 {
+
+    public static int longestValidParentheses(String s) {
+           return 0;
+        }
+
+    public static void main(String[] args) {
+        System.out.println(longestValidParentheses("()())"));
+    }
+}
