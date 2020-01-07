4fbe19b55f7fe52deacc0389fd43d9ad3cfd4774
lihaoyu <825091124@qq.com>
2020-01-07 22:03:28 +0800

    第二天安装软件，等待代码库权限

--- src/main/java/algorithm/temp/Main2.java
+++ src/main/java/algorithm/temp/Main2.java
@@ -11,21 +11,57 @@ public class Main2 {
 
     private static List<List<Integer>> res = new ArrayList<>();
     private static List<Integer> temp = new ArrayList<>();
-    private static List<Integer> list1 = new ArrayList<>();
-    private static List<Integer> list2 = new ArrayList<>();
+    private static int[] nums = new int[]{1, 2, 3, 4};
+    private static boolean[] flags = new boolean[nums.length];
+    private static int selectNum = 2;
 
-    private void fun(int i, int j){
-        if(i == 3 && j == 3){
-
+    // 全排列, 基于交换
+    private static void fun(int i) {
+        if (i == nums.length) {
+            temp = new ArrayList<>();
+            for (int k = 0; k < nums.length; k++) {
+                temp.add(nums[k]);
+            }
+            res.add(temp);
+            return;
+        }
+        for (int j = i; j < nums.length; j++) {
+            swap(i, j);
+            fun(i + 1);
+            swap(i, j);
         }
     }
 
+    private static void swap(int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // 全排列， 基于 flag
+    private static void fun2() {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!flags[i]) {
+                temp.add(i + 1);
+                flags[i] = true;
+                fun2();
+                flags[i] = false;
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+
+    // 选择数
+
+
     public static void main(String[] args) {
-        list1.add(1);
-        list1.add(2);
-        list1.add(3);
-        list2.add(4);
-        list2.add(5);
-        list2.add(6);
+        fun2();
+        for (int i = 0; i < res.size(); i++) {
+            System.out.println(res.get(i));
+        }
     }
 }
