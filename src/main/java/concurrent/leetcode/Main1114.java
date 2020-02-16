66fa1a7f33c40f56845d35c9e7cdcbfe1ffedd72
lihaoyu <825091124@qq.com>
2020-02-16 21:31:22 +0800

    .

--- src/main/java/concurrent/leetcode/Main1114.java
+++ src/main/java/concurrent/leetcode/Main1114.java
@@ -1,14 +1,21 @@
 package concurrent.leetcode;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ *  1114. 按序打印  三个线程。 三种方法，原始 sync  atomicInteger    countDownLatch
  * @author lihaoyu
  * @date 2020/2/14 22:56
  */
 class Foo {
 
-    volatile int flag = 0;
+    int flag = 0;
+
+    private AtomicInteger index = new AtomicInteger(0);
+
+    private CountDownLatch latch1 = new CountDownLatch(1);
+    private CountDownLatch latch2 = new CountDownLatch(1);
 
     public Foo() {
 
@@ -16,39 +23,56 @@ class Foo {
 
     public void first(Runnable printFirst) throws InterruptedException {
 
-        synchronized (this){
-            while(flag != 0){
-                this.wait();
-            }
-            printFirst.run();
-            flag++;
-            this.notifyAll();
-        }
-        // printFirst.run() outputs "first". Do not change or remove this line.
+//        synchronized (this){
+//            while(flag != 0){
+//                this.wait();
+//            }
+//            printFirst.run();
+//            flag++;
+//            this.notifyAll();
+//        }
+
+//        while(index.get() != 0);
+//        printFirst.run();
+//        index.incrementAndGet();
+
+        printFirst.run();
+        latch1.countDown();
 
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        synchronized (this){
-            while(flag != 1){
-                this.wait();
-            }
-            printSecond.run();
-            flag++;
-            this.notifyAll();
-        }
+//        synchronized (this){
+//            while(flag != 1){
+//                this.wait();
+//            }
+//            printSecond.run();
+//            flag++;
+//            this.notifyAll();
+//        }
+        while(index.get() != 1);
+        printSecond.run();
+        index.incrementAndGet();
+
+        latch1.await();
+        printSecond.run();
+        latch2.countDown();
 
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        synchronized (this){
-            while(flag != 2){
-                this.wait();
-            }
-            printThird.run();
-            flag++;
-            this.notifyAll();
-        }
+//        synchronized (this){
+//            while(flag != 2){
+//                this.wait();
+//            }
+//            printThird.run();
+//            flag++;
+//            this.notifyAll();
+//        }
+        while(index.get() != 2);
+        printThird.run();
+        index.incrementAndGet();
+
     }
 }
 
---
+++ src/main/java/concurrent/temp/ReadAndWriteTest.java
@@ -0,0 +1,15 @@
+package concurrent.temp;
+
+
+/**
+ * @author lihaoyu
+ * @date 2020/2/16 14:58
+ */
+public class ReadAndWriteTest {
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
