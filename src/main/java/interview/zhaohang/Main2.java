9130b2eba815337be09f0ad72ea644a1dcc83c8a
haoyli <825091124@qq.com>
2020-04-17 11:21:44 +0800

    华为机试

---
+++ src/main/java/interview/zhaohang/Main2.java
@@ -0,0 +1,25 @@
+package interview.zhaohang;
+
+import java.util.List;
+
+/**
+ * @author lihaoyu
+ * @date 2020/4/8 20:05
+ */
+public class Main2 {
+
+    // 从数 1-n 中选 M 个
+    private static int[] cFun(int n, int m){
+        int[] res = new int[m];
+        return null;
+    }
+
+    private static void cPart(int n, int m, List<Integer> res){
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
