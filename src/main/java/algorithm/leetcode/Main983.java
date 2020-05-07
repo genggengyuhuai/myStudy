d0f14785763fcfb8ee986e63bfd6235693d941a3
lihaoyu.francis <lihaoyu.francis@bytedance.com>
2020-05-07 23:57:56 +0800

    leetcode every day
    Change-Id: I6121e87b4bdba3985d8e550e02445e20fe7f69db

--- src/main/java/algorithm/leetcode/Main983.java
+++ src/main/java/algorithm/leetcode/Main983.java
@@ -1,8 +1,74 @@
 package algorithm.leetcode;
 
+
 /**
  * @author lihaoyu
  * @date 2020/5/6 23:42
  */
 public class Main983 {
+
+    public static int mincostTickets(int[] days, int[] costs) {
+        int[] dp = new int[days.length];
+        int[] flag = new int[days.length];
+        dp[0] = costs[0];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + costs[0];
+        }
+        for (int i = 0; i < dp.length; i++) {
+            // 是买7天，还是30天，还是不动
+            int index7 = getIndex(days, days[i] - 7 + 1);
+            int seven = dp[index7] - costs[flag[index7]] + costs[1];
+            int index30 = getIndex(days, days[i] - 30 + 1);
+            int thirty = dp[index30] - costs[flag[index30]] + costs[2];
+            // 不动
+            if (dp[i] < seven && dp[i] < thirty){
+                if(i == 0) continue;
+                dp[i] = dp[i-1] + costs[0];
+            }
+            // 7天
+            else if (seven <= dp[i] && seven < thirty) {
+                flag[i] = 1;
+                for (int j = 0; j < dp.length; j++) {
+                    if (days[index7] + 7 <= days[j]) break;
+                    dp[j] = Math.min(dp[j],seven);
+                }
+            } else {
+                flag[i] = 2;
+                for (int j = 0; j < dp.length; j++) {
+                    if (days[index7] + 30 <= days[j]) break;
+                    dp[j] = Math.min(dp[j],thirty);
+                }
+            }
+        }
+        return dp[dp.length-1];
+    }
+/*
+[1,4,6,7,8,20]
+[2,7,15]
+
+[1,2,3,4,5,6,7,8,9,10,30,31]
+[2,7,15]
+*/
+    private static int getIndex(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= k) return i;
+        }
+        // 不存在
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(mincostTickets(new int[]{1,2,3,4,5,6,7,8,9,10,30,31}
+        ,new int[]{2,7,15}));
+    }
 }
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/temp/Order.java
@@ -0,0 +1,65 @@
+package temp;
+
+import org.apache.htrace.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author lihaoyu
+ * @date 2020/5/7 8:24 下午
+ */
+public class Order implements Delayed {
+    /**
+     * 延迟时间
+     */
+    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    private long time;
+    String name;
+
+    public Order(String name, long time, TimeUnit unit) {
+        this.name = name;
+        this.time = System.currentTimeMillis() + (time > 0 ? unit.toMillis(time) : 0);
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return time - System.currentTimeMillis();
+    }
+    @Override
+    public int compareTo(Delayed o) {
+        Order Order = (Order) o;
+        long diff = this.time - Order.time;
+        if (diff <= 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+   //上边只是简单的实现入队与出队的操作，实际开发中会有专门的线程，负责消息的入队与消费。
+    public static void main(String[] args) throws InterruptedException {
+        Order Order1 = new Order("Order1", 5, TimeUnit.SECONDS);
+        Order Order2 = new Order("Order2", 10, TimeUnit.SECONDS);
+        Order Order3 = new Order("Order3", 15, TimeUnit.SECONDS);
+        DelayQueue<Order> delayQueue = new DelayQueue<>();
+        delayQueue.put(Order1);
+        delayQueue.put(Order2);
+        delayQueue.put(Order3);
+
+        System.out.println("订单延迟队列开始时间:" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        while (delayQueue.size() != 0) {
+            /**
+             * 取队列头部元素是否过期
+             */
+            Order task = delayQueue.poll();
+            if (task != null) {
+                System.out.format("订单:{%s}被取消, 取消时间:{%s}\n", task.name, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            }
+            Thread.sleep(1000);
+        }
+    }
+}
