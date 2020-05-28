019e8a91a98467734e505b3f647a41133266c1a4
lihaoyu.francis <825091124@qq.com>
2020-05-28 23:15:37 +0800

    lc ervery day
    Change-Id: Ibea9d5c467c81fd662e0a6be0918974242e214ee

---
+++ src/main/java/algorithm/leetcode/Main394.java
@@ -0,0 +1,22 @@
+package algorithm.leetcode;
+
+/**
+ * 字符串解码
+ * s = "3[a]2[bc]", 返回 "aaabcbc".
+ * s = "3[a2[c]]", 返回 "accaccacc".
+ * s = "2[abc]3[cd]ef", 返回 "abcabccdcdcdef".
+ * @author lihaoyu
+ * @date 2020/5/28 12:40 下午
+ */
+public class Main394 {
+
+
+    public String decodeString(String s) {
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
