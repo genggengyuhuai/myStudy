2fd63c519aa90e1a8ab62e61429d53eb4b0c5612
lihaoyu.francis <825091124@qq.com>
2020-07-05 10:26:27 +0800

    bytedance题库 数组与排序
    Change-Id: I3ccec7e6188d4e88276e3f6ca51443d65c5c8a9e

--- src/main/java/algorithm/leetcode/bytedance/array_sort/Main6.java
+++ src/main/java/algorithm/leetcode/bytedance/array_sort/Main6.java
@@ -1,8 +1,27 @@
 package algorithm.leetcode.bytedance.array_sort;
 
 /**
+ *   最长连续序列
+ * 给定一个未排序的整数数组，找出最长连续序列的长度。
+ * 要求算法的时间复杂度为 O(n)。
+ * 示例:
+ * 输入: [100, 4, 200, 1, 3, 2]
+ * 输出: 4
+ * 解释: 最长连续序列是 [1, 2, 3, 4]。它的长度为 4。
  * @author lihaoyu
  * @date 2020/7/4 7:57 下午
  */
 public class Main6 {
+
+
+    public int longestConsecutive(int[] nums) {
+
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
