602d5c279018367166593ced168c53c8120f6b55
lihaoyu.francis <825091124@qq.com>
2020-07-30 07:16:22 +0800

    bytedance
    Change-Id: Icd2e309ecd1a59bc19a23a773ef1e4089c82ffb8

--- src/main/java/algorithm/leetcode/bytedance/linkedList_tree/Main5.java
+++ src/main/java/algorithm/leetcode/bytedance/linkedList_tree/Main5.java
@@ -5,4 +5,25 @@ package algorithm.leetcode.bytedance.linkedList_tree;
  * @date 2020/7/28 12:17 AM
  */
 public class Main5 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public ListNode detectCycle(ListNode head) {
+
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/algorithm/leetcode/bytedance/linkedList_tree/Main6.java
+++ src/main/java/algorithm/leetcode/bytedance/linkedList_tree/Main6.java
@@ -1,8 +1,47 @@
 package algorithm.leetcode.bytedance.linkedList_tree;
 
 /**
+ * 找链表交点 可能无
  * @author lihaoyu
  * @date 2020/7/28 12:17 AM
  */
 public class Main6 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode lastA = headA, lastB = headB;
+        int lenA = 1, lenB = 1;
+        while(lastA.next != null){
+            lenA++;
+            lastA = lastA.next;
+        }
+        while(lastB.next != null){
+            lenB++;
+            lastB = lastB.next;
+        }
+        if(lastA != lastB) return null;
+        ListNode fast = lenA >= lenB ? headA : headB, low = lenA >= lenB ? headB : headA;
+        // fast 先走
+        for (int i = 0; i < Math.abs(lenA - lenB); i++) {
+            fast = fast.next;
+        }
+        while(fast != low){
+            fast = fast.next;
+            low = low.next;
+        }
+        return fast;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
