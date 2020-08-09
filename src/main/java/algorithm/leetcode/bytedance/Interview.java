87141e5611c39293a7871b7b72a4078e8be46677
lihaoyu.francis <825091124@qq.com>
2020-08-09 22:42:33 +0800

    Git rebase and merge
    Change-Id: I75149a448de652fbc8e21e8a46f728853ff9a4dc

--- src/main/java/algorithm/leetcode/bytedance/Interview.java
+++ src/main/java/algorithm/leetcode/bytedance/Interview.java
@@ -4,6 +4,7 @@ package algorithm.leetcode.bytedance;
  * 字节跳动后端实习生 2020.3.2 三面 匡振国
  * 排序数组  -5,-3, -2,-2,1,1,2,2,3,4 输出每个
  * 元素平方后有多少个不同的元素  空间O(1)
+ *
  * @author lihaoyu
  * @date 2020/8/3 9:55 下午
  */
@@ -24,13 +25,13 @@ public class Interview {
                 temp = absRight;
             } else {
             }
-
-
-            return res;
         }
 
-        public static void main (String[]args){
 
-        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
