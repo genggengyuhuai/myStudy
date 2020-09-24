1b286e4bd9755a6bc5ef9f74202e26e0efd485f2
lihaoyu.francis <825091124@qq.com>
2020-09-24 23:56:40 +0800

    ms warming up
    Change-Id: Ief1eb180df15891853b925fa554bb40573b13243

---
+++ src/main/java/interview/ms/Main1.java
@@ -0,0 +1,17 @@
+package interview.ms;
+
+/**
+ * 2020.9.25 上午 11点 ms 苏州 STCA 面试
+ * @author lihaoyu
+ * @date 2020/9/24 11:06 下午
+ */
+public class Main1 {
+
+    
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
