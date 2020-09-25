5fdd2d2637bdd3467016416c9b07e6995b484b88
lihaoyu.francis <825091124@qq.com>
2020-09-25 16:24:16 +0800

    ms 纯聊天
    Change-Id: Iefbf7645976de7f26f90864c764d79d325972e0b

--- src/main/java/interview/ms/Main1.java
+++ src/main/java/interview/ms/Main1.java
@@ -7,7 +7,19 @@ package interview.ms;
  */
 public class Main1 {
 
-    
+    private static void quickSort(int[] nums, int left, int right){
+        if(left >= right) return;
+        int pivot = nums[left], start = left, end = right;
+        while(start < end){
+            while(start < end && nums[end] >= pivot) end--;
+            nums[start] = nums[end];
+            while(start < end && nums[start] <= pivot) start++;
+            nums[end] = nums[start];
+        }
+        nums[start] = pivot;
+        quickSort(nums,left,start-1);
+        quickSort(nums,start+1,end);
+    }
 
     public static void main(String[] args) {
 
