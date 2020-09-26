241af5c299e97b967f18cf7f7395b9a1476d9743
lihaoyu.francis <825091124@qq.com>
2020-09-27 00:01:23 +0800

    tecent 9.26 pcg first interview
    Change-Id: Ia91170009ad9d68729c16f8199a4fce4068d4453

---
+++ src/main/java/interview/tecent/Main21.java
@@ -0,0 +1,45 @@
+package interview.tecent;
+
+import java.util.LinkedList;
+
+/**
+ * @author lihaoyu
+ * @date 2020/9/26 10:23 上午
+ */
+public class Main21 {
+
+    private static class Node{
+        int key;
+        Node right;
+        Node left;
+    }
+
+    private static void recursiveInOrderVisit(Node root){
+        if(root == null) return;
+        recursiveInOrderVisit(root.left);
+        System.out.println(root.key+" ");
+        recursiveInOrderVisit(root.right);
+    }
+
+    private static void noRecursiveInOrderVisit(Node root){
+        if(root == null) return;
+        LinkedList<Node> list = new LinkedList<>();
+        while(root != null || !list.isEmpty()){
+            while(root != null){
+                list.addLast(root);
+                root = root.left;
+            }
+            if(!list.isEmpty()){
+                root = list.pollLast();
+                System.out.println(root.key);
+                root = root.right;
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
