40556bbe536f9ce643a07638e4eb620a9233474f
lihaoyu.francis <825091124@qq.com>
2020-10-13 23:28:43 +0800

    22forthdate
    Change-Id: I209da5381a26dfef155771d35832ec86cd5646ca

--- src/main/java/interview/tecent/Main21.java
+++ src/main/java/interview/tecent/Main21.java
@@ -36,7 +36,6 @@ public class Main21 {
             }
         }
     }
-
     private static void noRecursivePostOrderVisit(Node root){
         if(root == null) return;
         LinkedList<Node> list = new LinkedList<>();
