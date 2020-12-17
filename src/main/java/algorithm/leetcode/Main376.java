ed3112cd4451ae61ab3570507b31932200167960
DESKTOP-haoyli <825091124@qq.com>
2020-12-18 00:00:28 +0800

    1

--- src/main/java/algorithm/leetcode/Main376.java
+++ src/main/java/algorithm/leetcode/Main376.java
@@ -18,7 +18,6 @@ public class Main376 {
        int[] up = new int[nums.length], down = new int[nums.length];
        up[0] = 1;
        down[0] = 1;
-
        int  res = 1;
         for (int i = 1; i < nums.length; i++) {
             if(nums[i] > nums[i-1]){
