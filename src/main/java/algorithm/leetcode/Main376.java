91fd49abe45eb6584a0b009e53bb59668129996f
DESKTOP-haoyli <825091124@qq.com>
2021-01-07 00:01:16 +0800

    2

--- src/main/java/algorithm/leetcode/Main376.java
+++ src/main/java/algorithm/leetcode/Main376.java
@@ -29,6 +29,7 @@ public class Main376 {
                 down[i] = down[i-1];
                 up[i] = up[i-1];
             }
+
             res = Math.max(res,Math.max(down[i],up[i]));
         }
 
